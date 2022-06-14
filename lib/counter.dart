87129efd920b6f7a003f967adca6f43c4ea2d4c6
Sanjukumar <sanjukumarpukale98@gmail.com>
2022-06-14 16:41:48 +0530

    counter app using mobx

---
+++ lib/counter.dart
@@ -0,0 +1,14 @@
+import 'package:mobx/mobx.dart';
+
+class Counter {
+
+  Observable counter = Observable(0);
+
+  Action? increment;
+
+  Counter() {
+    increment = Action(() {
+      counter.value++;
+    });
+  }
+}
